625a691595d3c2e10866f1c83cbf505b101a94ab
mt-satak <sakiware@gmail.com>
2021-01-16 02:37:57 +0900

    仮トップページのビューを定義

--- kanban/kanban/views.py
+++ kanban/kanban/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    """
+    トップページの初期表示アクション
+    indexというビューを定義
+    """
+    # テンプレートを指定せずに文字列を直接指定 文字列をレスポンスとして返すときはHttpResponseを使用する
+    return HttpResponse('仮のトップページ')
