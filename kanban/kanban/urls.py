b0c24efa05da81620f612aca0e6644efb6f86756
mt-satak <sakiware@gmail.com>
2021-01-24 05:54:00 +0900

    編集/削除機能：ビュー・ルーティング設定

--- kanban/kanban/urls.py
+++ kanban/kanban/urls.py
@@ -27,4 +27,6 @@ urlpatterns = [
     path('cards/', views.CardListView.as_view(), name='cards_list'),
     path('cards/create/', views.CardCreateView.as_view(), name='cards_create'),
     path('cards/<int:pk>/', views.CardDetailView.as_view(), name='cards_detail'),
+    path('cards/<int:pk>/update/', views.CardUpdateView.as_view(), name='cards_update'),
+    path('cards/<int:pk>/delete/', views.CardDeleteView.as_view(), name='cards_delete'),
 ]
